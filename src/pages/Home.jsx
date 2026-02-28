c56da8c51998639e9fec35cb5d1a34bfa5ac6039
xpctncy <veixzd@gmail.com>
2026-02-28 01:19:47 +0000

    complete + github pages setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function Home() {
+
+  const [submittedBux, setSubmittedBux] = useState(null);
+
+  async function FormHandler(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const user = formData.get("username");
+    const bux = formData.get("bux-amount");
+
+    if (user?.length && bux?.length > 0) {
+      setSubmittedBux(bux);
+    } else {
+      console.log("Invalid details");
+    }
+  }
+
+  return (
+    <>
+    <h1 className="bux-title">Noglux bux generator</h1>
+    <div className="bux-container">
+        <form onSubmit={FormHandler} className="bux-form">
+            <input type="text" name="username" placeholder="Username" className="bux-input"></input>
+            <input type="number" name="bux-amount" placeholder="Bux Amount" min="1" max="1000000" className="bux-input"></input>
+            <p className="bux-sign">FREE 🤑🤑🤑</p>
+            <input type="submit" value="Receive" className="bux-submit"></input>
+        </form>
+    </div>
+    {submittedBux && (
+    <div className="submit-notification">
+        <button className="submit-close" onClick={() => setSubmittedBux(null)}>x</button>
+        <p className="notification-header">Success!</p>
+        <img src="./assets/images/noglux-bux.png" className="noglux-bux"></img>
+        <p className="notification-text">
+        <span className="bux-highlight">
+            {submittedBux}
+        </span>{" "}
+        Bux have been deposited to your Noglux account!
+        </p>
+    </div>  
+    )}
+    <h6 className="site-warning">Obviously this is a joke website, you will not get free bux. </h6>
+    </>
+  );
+}
